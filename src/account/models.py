ed59fee66e0da5dc9de38e6f6ec9df1eb8d7424d
javad <m.g.gh9121@gmail.com>
2021-01-07 14:59:02 +0330

    use signals for account (extend bio and age) / create view & url & templates for category

--- src/account/models.py
+++ src/account/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+# ایمپورت خط 3 برای اضافه کردن اطلاعات کلاس پروفایل به یوزر است تا بدین ترتیب بعد از سیو و ایجاد هر یوزر این
+# آپشن ها اضافه شود.
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(null=True, blank=True)
     age = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+# signals(جلسه8):
+def save_profile(sender, **kwargs):
+    if kwargs['created']:
+        p1 = Profile(user=kwargs['instance'])
+        p1.save()
+
+
+post_save.connect(save_profile, sender=User)
+# از طریق خط بالا به یوزر وصل شده و اطلاعات یوزر رو میگیریم
+# post_save دو مقدار created و instance رو برمیگردونه
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render, get_object_or_404
-from . models import Post
+from .models import Post
 
 
 def all_posts(request):
     posts = Post.objects.all()
     return render(request, 'posts/all_posts.html', {'posts': posts})
 
+
+# ---------------------category views---------------------------------
+def post_darsi(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/category/darsi.html', {'posts': posts})
+
+
+def post_eghtesadi(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/category/eghtesadi.html', {'posts': posts})
+
+
+def post_ejtemaei(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/category/ejtemaei.html', {'posts': posts})
+
+
+def post_farhango_honar(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/category/farhango honar.html', {'posts': posts})
+
+
+def post_siyasi(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/category/siyasi.html', {'posts': posts})
+
+
+# -------------------------------------------------------------------------------
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, created__year=year, created__month=month, created__day=day,
